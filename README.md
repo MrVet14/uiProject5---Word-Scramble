d4b3d217aa3dd732ceb6dd9b64ca5ea883d2cc58
Vitali <vitali.vyucheiski@gmail.com>
2022-06-27 20:11:46 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# uiProject5---Word-Scramble
+ A Learning Project 5, coded by Me during Hacking with SwiftUI in 100 Days Course
---
+++ uiProject5 - Word Scramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  uiProject5 - Word Scramble
+//
+//  Created by Vitali Vyucheiski on 6/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
